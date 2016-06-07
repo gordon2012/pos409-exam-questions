cd1aaf3840597bb62f4d37eba57884bcb3e66bb0
Gordon Doskas <gordon2012@users.noreply.github.com>
2016-06-07 13:59:57 -0700

    initial commit

---
+++ pos409-exam-questions/MainForm.Designer.cs
@@ -0,0 +1,159 @@
+﻿namespace pos409_exam_questions
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.pnlStudents = new System.Windows.Forms.Panel();
+            this.tbrStudents = new System.Windows.Forms.TrackBar();
+            this.lblStudents = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.btnGenerate = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.tbrStudents)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(63, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(49, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Students";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(232, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(49, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Chapters";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(287, 6);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(126, 20);
+            this.textBox2.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(42, 437);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(96, 52);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pnlStudents
+            // 
+            this.pnlStudents.Location = new System.Drawing.Point(42, 141);
+            this.pnlStudents.Name = "pnlStudents";
+            this.pnlStudents.Size = new System.Drawing.Size(203, 249);
+            this.pnlStudents.TabIndex = 6;
+            // 
+            // tbrStudents
+            // 
+            this.tbrStudents.LargeChange = 1;
+            this.tbrStudents.Location = new System.Drawing.Point(42, 71);
+            this.tbrStudents.Minimum = 1;
+            this.tbrStudents.Name = "tbrStudents";
+            this.tbrStudents.Size = new System.Drawing.Size(168, 45);
+            this.tbrStudents.TabIndex = 7;
+            this.tbrStudents.Value = 6;
+            this.tbrStudents.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            // 
+            // lblStudents
+            // 
+            this.lblStudents.AutoSize = true;
+            this.lblStudents.Location = new System.Drawing.Point(118, 28);
+            this.lblStudents.Name = "lblStudents";
+            this.lblStudents.Size = new System.Drawing.Size(28, 13);
+            this.lblStudents.TabIndex = 8;
+            this.lblStudents.Text = "###";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(314, 185);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(258, 271);
+            this.textBox1.TabIndex = 9;
+            // 
+            // btnGenerate
+            // 
+            this.btnGenerate.Location = new System.Drawing.Point(404, 112);
+            this.btnGenerate.Name = "btnGenerate";
+            this.btnGenerate.Size = new System.Drawing.Size(133, 39);
+            this.btnGenerate.TabIndex = 10;
+            this.btnGenerate.Text = "Generate";
+            this.btnGenerate.UseVisualStyleBackColor = true;
+            this.btnGenerate.Click += new System.EventHandler(this.btnGenerate_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(689, 555);
+            this.Controls.Add(this.btnGenerate);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.lblStudents);
+            this.Controls.Add(this.tbrStudents);
+            this.Controls.Add(this.pnlStudents);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "MainForm";
+            this.Text = "Exam Question Assigner";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.tbrStudents)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel pnlStudents;
+        private System.Windows.Forms.TrackBar tbrStudents;
+        private System.Windows.Forms.Label lblStudents;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button btnGenerate;
+    }
+}
+
---
+++ pos409-exam-questions/MainForm.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace pos409_exam_questions
+{
+    public partial class MainForm : Form
+    {
+
+        //int students = 0;
+
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+
+        private void ResizeStudents()
+        {
+            //pnlStudents.
+
+            //for (int i = 0; i < tbrStudents.Value; i++)
+            //{
+            //    TextBox newTxt = new TextBox();
+
+            //    newTxt.Parent = pnlStudents;
+            //    newTxt.Left = 10;
+            //    newTxt.Top = 10 + 25 * i;
+            //}
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //int test = 1;
+
+            //Console.WriteLine(test);
+
+            //TextBox newTxt = new TextBox();
+            //newTxt.Location.X = 0;
+
+            //newTxt.Left = 0;
+            //newTxt.Top = 20 * students;
+
+            //newTxt.Parent = gbxStudents;
+
+            //students++;
+        }
+
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            lblStudents.Text = tbrStudents.Value.ToString();
+            ResizeStudents();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            lblStudents.Text = tbrStudents.Value.ToString();
+        }
+
+        private void btnGenerate_Click(object sender, EventArgs e)
+        {
+            string[] chapterNames = { "W2Ch3", "W2Ch4", "W2Ch5", "W2Ch8", "W3Ch6", "W3Ch7", "W4Ch9", "W4Ch10", "W5Ch11", "W5Ch12"};
+            int[] chapterCounts = {15,13,14,20,21, 10,20,13,14,10};
+
+
+
+
+        }
+    }
+}
